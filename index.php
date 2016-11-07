080749a66e1e5057d480126299fa7c7706059208
er1an9 <sun_eagle@ymail.com>
2016-11-07 11:44:29 +0700

    tambah jquery

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href='materialize/css/materialize.min.css'>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<nav>
+  <div class="nav-wrapper">
+    <a href="#" class="brand-logo">Logo</a>
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Components</a></li>
+      <li><a href="#">Login</a></li>
+    </ul>
+  </div>
+</nav>
+
+<div class="row">
+	<div class="col s3">
+		<ul class="collection with-header">
+      <li class="collection-header"><h4>Menu Utama</h4></li>
+      <li class="collection-item">
+      	<div>Home<a href="#!" class="secondary-content"><i class="material-icons">home</i></a></div>
+      </li>
+      <li class="collection-item">
+      	<div>Berita<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div>
+      </li>
+      <li class="collection-item">
+      	<div>Agenda<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div>
+      </li>
+      <li class="collection-item">
+      	<div>Profile<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div>
+      </li>
+    </ul>
+	 </div>
+	<div class="col s9">sis4</div>
+</div>
+
+	<script src="materialize/js/jquery.min.js"></script>
+	<script src="materialize/js/materialize.min.js"></script>
+</body>
+</html>
